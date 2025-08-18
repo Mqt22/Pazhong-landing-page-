3ba4e9a6174638cff0d81b604a186bfbb08a9bc5
Abdul Muqeet Shahzad <F2023332001@umt.edu.pk>
2025-08-18 19:54:46 +0500

    Initial commit for Flask project

---
+++ static/js/nav.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav>ul');
+const cta = document.querySelector('header>button');
+hamburger.addEventListener('click', ()=>{
+    nav.classList.toggle('active');
+    cta.classList.toggle('active');
+});
+// const hamburger = document.querySelector('.hamburger');
+// const nav = document.querySelector('nav');
+// const nav_link = document.querySelector('nav>ul');
+// hamburger.addEventListener('click', ()=>{
+//     nav.classList.toggle('active');
+//     nav_link.classList.toggle('active');
+// });
